57960ba1b10b913c3de6ebe62844236fe03940ad
Tristán Marín <tristanmarinlopez@alumnos.tuniversformacion.es>
2023-10-03 10:28:06 +0200

    Identificador de numeros

---
+++ index.js
@@ -0,0 +1,12 @@
+let num = prompt('Introduzca un numero: ')
+numero = parseInt(num);
+if (numero>0){
+    alert('Numero positivo')
+    console.log('El numero es positivo')
+}else if(numero<0){
+    alert('Numero negativo')
+    console.log('El numero es negativo')
+}else{
+    alert('El numero es 0')
+    console.log('El numero es 0')
+}
